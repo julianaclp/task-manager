cd043c3fbe047eb3153480f47a96ff95a82b74ff
julianaclp <julianaclp350@gmail.com>
2018-09-05 09:06:21 -0300

    .gitignore e algumas verificações no tempo de execução

--- tmanager.cpp
+++ tmanager.cpp
@@ -7,15 +7,15 @@ int main(int argc, char const *argv[]) {
   srand((unsigned int)time(NULL));
   int execution_time, memory_size, task_size, speed, max_delta, i=0;
   int *memory;
-  float delta_time, val;
+  float delta_time, val, total_time = 0.00;
   printf("Digite o tempo de execucao da simulacao\n");
   scanf("%d", &execution_time);
-  printf("Digite o tamanho maximo da memoria\n");
-  scanf("%d", &memory_size);
+  //printf("Digite o tamanho maximo da memoria\n");
+  //scanf("%d", &memory_size);
   printf("Digite o intervalo maximo de execucao das tarefas\n");
   scanf("%d", &max_delta);
-  printf("Digite o tempo necessario para a execucao de um byte da tarefa\n");
-  scanf("%d", &speed);
+  /*printf("Digite o tempo necessario para a execucao de um byte da tarefa\n");
+  scanf("%d", &speed);*/
 
   /*Aloca espaço da memória*/
   memory = (int *) malloc(sizeof(int)*memory_size);
@@ -23,9 +23,12 @@ int main(int argc, char const *argv[]) {
   /*Execução das tarefas em tempo aleatório*/
   while(1)
   {
-    val = (((float)rand()) / RAND_MAX)*max_delta;
+    delta_time = (((float)rand()) / RAND_MAX)*max_delta;
     /*Executar as tarefas aqui*/
-    sleep(val);
+    total_time += delta_time;
+    printf("%f\n", total_time);
+    if(total_time > execution_time) break;
+    sleep(delta_time);
   }
   return 0;
 }
